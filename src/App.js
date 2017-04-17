a30ee7cb4dc51bace6cf1f97d5916bae991f8ae0
njdaniel <nicholasjdaniel@gmail.com>
2017-04-17 17:36:28 -0400

    youtube search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyC1OhMfjQ0-i5dWT829dcF_gd4ZKoStlZI';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
 
 class App extends Component {
   render() {
@@ -8,11 +16,12 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to VideoSync</h2>
         </div>
         <p className="App-intro">
-          Hello
+          Search Videos
         </p>
+          <SearchBar/>
       </div>
     );
   }
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,3 +1,25 @@
 /**
  * Created by nicholas on 4/17/17.
  */
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''};
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({term: event.target.value})}/>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
