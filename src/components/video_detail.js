ad991591f47b392ac46d0c9ee459806cac498c20
njdaniel <nicholasjdaniel@gmail.com>
2017-04-19 16:51:29 -0400

    add youtube url

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,3 +1,21 @@
 /**
  * Created by nicholas on 4/17/17.
  */
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+    const videoId = video.id.videoId;
+    const url = `https:www.youtube.com/embed/${videoId}`;
+
+    return (
+        <div className="video-detail col-md-8">
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-responsive-item"></iframe>
+            </div>
+            <div className="details">
+                <div>{video.snippet.title}</div>
+                <div>{video.snippet.description}</div>
+            </div>
+        </div>
+    );
+};
